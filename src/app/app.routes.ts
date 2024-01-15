178f714cc55686a5619ba5b18883b9e8829d1ca0
Lalit <lalit2506verma@gmail.com>
2024-01-15 14:04:25 +0530

    Initial Setup of the project(Base)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'signup',
+        component : RegistrationComponent,
+        pathMatch : 'full'
+    }
+];
